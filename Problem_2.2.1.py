04449e4555c3db515afb90ed20dbff05202ac634
wi11yv <144940670+wi11yv@users.noreply.github.com>
2024-09-30 17:12:26 +0000

    Quiz

---
+++ Problem_2.2.1.py
@@ -0,0 +1,88 @@
+#my_string_1 = "Hello!"
+#my_string_2 = "My name is David."
+#my_string_3 = "Welcome to CS1301!"
+#print(my_string_1)
+#print(my_string_3)
+#print(my_string_2)
+
+#from datetime import date
+#my_var_1 = True
+#my_var_2 = date.today()
+#my_var_3 = my_var_2.day
+#my_var_4 = 0.5
+
+#print(my_var_1)
+#print(my_var_2)
+#print(my_var_3)
+#print(my_var_4)
+
+#amount_of_$ = 10
+#print(amount_of_$)
+
+#my_var_1 = 5
+#my_var_1 = 3
+#my_var_2 = my_var_1
+#my_var_3 = "Hello!"
+#my_var_1 = my_var_3
+#my_var_3 = 5.2
+#my_var_2 = True
+
+#print(my_var_1)
+#print(my_var_2)
+#print(my_var_3)
+
+#my_var_1 = 5
+#my_var_2 = 7
+#temp = my_var_1
+#my_var_1 = my_var_2
+#my_var_2 = temp
+#print(my_var_2)
+
+#string1 = "Hello!"
+#print(string1)
+#print(string2)
+#string2 = "Welcome to CS1301!"
+#"My name is David." = string3
+#print(string3)
+#print(string4)
+
+#my_integer = 3
+#my_float = 5.5
+#my_string = "Hello!"
+#my_boolean = False
+#a = my_boolean + my_integer
+#b = my_integer * my_string
+#c = my_float * my_boolean
+#d = my_float * my_integer
+
+#print(a)
+#print(b)
+#print(c)
+#print(d)
+
+#letter_of_the_day = "F"
+#print("Letter of the day: " + letter_of_the_day)
+
+#my_string_1 = "2"
+#my_string_2 = "-9.1"
+#my_string_3 = "Hello!"
+#print(int(my_string_1))
+#print(bool(my_string_2))
+#print(float(my_string_3))
+#print(int(my_string_2))
+#print(float(my_string_1))
+
+#user_input = input("Enter a number: ")
+#print("Your original input: " + user_input)
+#user_input_as_float = float(user_input)
+#print("Your input as a float:", user_input_as_float)
+#user_input_as_int = int(user_input)
+#print("Your input as an integer:", user_input_as_int)
+
+#float = 5.1
+#float(float)
+#print(float)
+
+continue = "5"
+continue_as_int = int(continue)
+print(continue_as_int)
