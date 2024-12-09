68094fb0b8219448e27353cbff27e826756149e2
wi11yv <144940670+wi11yv@users.noreply.github.com>
2024-12-09 16:39:19 +0000

    updates

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,2 @@
-some_list = ["I", "like", "shorts", "they're", "comfy", "and", "easy", "to", "wear"]
-for i in range(0, len(some_list)):
-    print(i, some_list[i])
+for aString in range(0):
+    print(aString)
