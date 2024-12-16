47c4a21d441edd57ce4115f2709a0e3a11a49966
wi11yv <144940670+wi11yv@users.noreply.github.com>
2024-12-16 18:55:57 +0000

    updates

--- sandbox.py
+++ sandbox.py
@@ -1,12 +1,9 @@
-def poke_go(name, number_of_pokemon = 0):
-     if number_of_pokemon == 0:
-         return "You have caught no Pokemon, " + name
-     elif number_of_pokemon < 5:
-         return "You are just starting out, " + name
-     elif number_of_pokemon < 250:
-         return "You are getting better, " + name
-     else:
-         return "You are the very best, like no one ever was, " + name
-print(poke_go("NewGamer98"))
-print(poke_go("PokeMASTER222", number_of_pokemon = 100))
-print(poke_go("GaryFOak", 250))
+i = -2
+j = 2
+while i < j:
+    try:
+        print(j // i)
+        i += 1
+    except:
+        print("An error occurred!")
+print("Done!")
