0dbe6af3e35596eba989b59d6a1ecef40bad93f0
wi11yv <144940670+wi11yv@users.noreply.github.com>
2024-12-21 01:20:19 +0000

    Unit 3 test

---
+++ 3.2.PartialFactorial.py
@@ -0,0 +1,26 @@
+minimum = 2
+maximum = 5
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+
+#Print the result of multiplying every number from minimum to
+#maximum, including both minimum and maximum.
+#
+#For example, if minimum = 2 and maximum = 5, you would print
+#120: 2 * 3 * 4 * 5 = 120.
+#
+#You may assume minimum will always be less than
+#maximum.
+
+
+#Add your code here!
+for i in range(minimum, maximum):
+    result = i * i
+print(result * i)
+
+
+
+
+
