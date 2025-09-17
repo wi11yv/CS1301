04c72d07b84ee58f02468d9ab431b55e34cb70b4
willyv <willrvarner@gmail.com>
2025-09-17 00:34:44 +0000

    Quick update

--- CS1301xIII/sandbox.py
+++ CS1301xIII/sandbox.py
@@ -1,9 +1,14 @@
-i = -2
-j = 2
-while i < j:
-    try:
-        print(j // i)
-        i += 1
-    except:
-        print("An error occurred!")
-print("Done!")
+phrase = "HULK MAD"
+phrase = phrase.lower()
+phrase = phrase.title()
+cut_phrase = phrase[:5]
+new_phrase = ""
+ 
+for char in phrase[5:]:
+    if char == "m":
+         new_phrase = cut_phrase + "Hungry"
+    elif char == "a":
+        new_phrase = cut_phrase + "Angry"
+    elif char == "d":
+         new_phrase = cut_phrase + "Smash"
+    print(new_phrase)
